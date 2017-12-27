ac8200f957d310df69c4f2b822a410e6ed0ae1fd
chisl <info@chisl.io>
2017-12-27 18:15:43 +0100

    Initial commit

---
+++ SX1276_77_78_79_FSK.h
@@ -0,0 +1,9 @@
+/*
+ * name:        SX1276_77_78_79
+ * description: 137 MHz to 1020 MHz Low Power Long Range Transceiver featuring the LoRa (TM) long range modem
+ * manuf:       Semtech
+ * version:     0.1
+ * url:         http://www.semtech.com/images/datasheet/sx1276_77_78_79.pdf
+ * date:        2016-08-01
+ */
+
